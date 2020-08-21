322abd67bb824b7b1e93c033cd2723721c025466
Abdul Ajetunmobi <abdul.ajetunmobi@vonage.com>
2020-08-21 13:11:59 +0100

    Initial Commit

---
+++ PhoneToApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PhoneToApp
+//
+//  Created by Abdulhakim Ajetunmobi on 21/08/2020.
+//  Copyright © 2020 Vonage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ PhoneToApp/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  PhoneToApp
+//
+//  Created by Abdulhakim Ajetunmobi on 21/08/2020.
+//  Copyright © 2020 Vonage. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
